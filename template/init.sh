cbac9e6f89892f4bc0786d7a0d54bb62dc03566c
Connor Hindley <conn.hindley@gmail.com>
2014-09-17 10:40:20 -0400

    add initial push to init script

--- template/init.sh
+++ template/init.sh
@@ -2,6 +2,8 @@
 #abort if any command fails
 set -o errexit
 
+initial_push=1
+
 # Parse arg flags
 while : ; do
 if [[ $1 = "-r" || $1 = "--repo" ]]; then
@@ -12,22 +14,39 @@ elif [[ $1 = "-t" || $1 = "--tag" ]]; then
     shift
     tag=$1
     shift
+elif [[ $1 = "--no-push" ]]; then
+    initial_push=0
+    shift
 else
     break
 fi
 done
 
+
+function status_message {
+    echo
+    echo "-----> $*"
+    echo
+}
+
 if [[ $repo && $tag ]]; then
     # Clean out directory
     find . type -f ! -name 'init.sh' | xargs rm
-    echo
-    echo "------> Downloading $repo at tag $tag"
-    echo
+
+    status_message "Downloading $repo at tag $tag"
+
     # Download release and extract here
     curl -Lk https://github.com/${repo}/archive/${tag}.tar.gz | tar -xz --strip-components=1
-    echo
-    echo "------> Finished! Enjoy!"
-    echo
+
+    if [ $initial_push -eq 1 ]; then
+        status_message "Doing initial push"
+        git add -A
+        git commit -m 'Initial boilerplate push'
+        git push origin
+    fi
+
+    status_message "Finished! Enjoy!"
+
     # Goodbye cruel world
     rm -- $0
 else
